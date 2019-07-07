30b84b0b825e1fc23e2e969778e11557432c0314
Johan <johan.aspenlind@b3.se>
2019-07-07 13:57:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# skynet-tools
+
+- A set of tools for the Skynet ASUS firewall (<https://github.com/Adamm00/IPSet_ASUS).>
+
+## vpn-whitelist-se.js
+
+- Script to generate a whitelist file with VPN ip addresses that can be imported to the firewall.
---
+++ vpn-whitelist-se.js
@@ -0,0 +1,17 @@
+const request = require("request");
+const fs = require("fs");
+
+request("https://api.nordvpn.com/server", { json: true }, (err, response, body) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  const ips = body
+    .filter(x => x.country === "Sweden")
+    .map(x => x.ip_address)
+    .join("\n");
+
+  fs.writeFile("file.txt", ips, e => {
+    if (e) console.error(e);
+  });
+});
