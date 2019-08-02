871d77361c3de5c084a69a11330717af6e224e84
Johan <johan.aspenlind@b3.se>
2019-08-02 16:28:42 +0200

    Ensure git is installed before cloning

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@ PACKAGE_DIR=$(mktemp -d)
 cd "$PACKAGE_DIR" || exit
 
 echo "asuswrt-cli installer"
-echo "==================="
+echo "====================="
+echo "==> Installing package manager…"
+curl -sSL "https://git.io/dependency-installer.sh" | bash
+
 echo "==> Downloading package…"
 git clone "https://github.com/jaspenlind/asuswrt-cli.git"
 
