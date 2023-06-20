b38ff1eb3e7b98313d4be0da7bc92afccab95faa
Johan <johan.aspenlind@b3.se>
2023-06-20 10:22:50 +0200

    bump deps

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   modulePathIgnorePatterns: ["<rootDir>/dist/"],
   preset: "ts-jest",
   roots: ["<rootDir>/src", "<rootDir>/test"],
-  setupFilesAfterEnv: ["jest-extended"],
+  setupFilesAfterEnv: ["jest-extended/all"],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   transform: {
     "^.+\\.tsx?$": "ts-jest"
